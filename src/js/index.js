3ae2e2350c03a760fadda1361b1c804d7fa3dfed
marisofip <marisofipalencia@gmail.com>
2022-12-19 16:37:40 +0000

    aleluya

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 //import react into the bundle
+import { number } from "prop-types";
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from 'prop-types'
 
 
 // include your styles into the webpack bundle
@@ -8,30 +10,41 @@ import "../styles/index.css";
 
 //funcion de counter
 function SecondsCounter(props){
-return ( 
+    return ( 
     <div className="contador">
     <div className="clock">
     <i className="far fa-clock"></i>
     </div>
-    <div className="decimalUno ">0</div>
-    <div className="decimalDos">0</div>
-    <div className="decimalTres">0</div>
-    <div className="decimalCuatro">0</div>
+    <div className="cuatro">{props.decimalCuatro % 10} </div>
+    <div className="tres">{props.decimalTres % 10} </div>
+    <div className="dos">{props.decimalDos % 10} </div>
+    <div className="uno">{props.decimalUno % 10} </div>
     </div>
 
 );
+}
+
+SecondsCounter.prototype ={
+    decimalCuatro: PropTypes.number,
+    decimalTres : PropTypes.number,
+    decimalDos: PropTypes.number,
+    decimalUno : PropTypes.number
 };
 
+let initial = 0; 
+setInterval(function(){
 
-/*let counter= 0;
-function contar(){
-    const uno = counter ++;
-    const dos = counter ++ ;
-    const tres = counter ++;
-    const cuatro = counter ++;
-counter++
-}, 1000*/
+    const cuatro = Math.floor(initial/1000);
+    const tres = Math.floor(initial/100);
+    const dos= Math.floor(initial/10);
+    const uno = Math.floor(initial/1);
+//console.log (uno, dos, tres, cuatro);
+    initial++; 
+
+    //render your react application
+    ReactDOM.render(<SecondsCounter 
+        decimalUno={uno} decimalDos={dos}  decimalTres={tres} decimalCuatro={cuatro} />, 
+        document.querySelector("#app"));
+    }, 1000);
 
 
-//render your react application
-ReactDOM.render(<SecondsCounter />, document.querySelector("#app"));
